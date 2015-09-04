4444b8cdc8454cfcfa49f68d5755404803a52f6c
Maor <m@m.m>
2015-09-04 18:18:00 +0300

    attempt to open WCF service to local network

---
+++ Lynes.ReportServer.Core/DB/CSVDBService.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lynes.ReportsServer.Core.DataModels;
+using System.IO;
+
+namespace Lynes.ReportsServer.Core.DB
+{
+    public class CSVDBService : IDBService
+    {
+        private Dictionary<string, StringBuilder> m_idToSB;
+
+        public CSVDBService()
+        {
+            m_idToSB = new Dictionary<string, StringBuilder>();
+        }
+
+        public void Init()
+        {
+            string fileName = GetNextFileName(string.Empty);
+            FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate);
+            StreamWriter sw = new StreamWriter(fs);
+        }
+
+        private string GetNextFileName(string id)
+        {
+            DateTime now = DateTime.UtcNow;
+            string result = string.Format("{0}.{1}.{2}_{3}.{4}.{5}.csv", now.Day, now.Month, now.Year, now.Hour, now.Minute, now.Second);
+            return result;
+        }
+
+
+
+        public void SaveAcceleration(AccelerometerData data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveLocation(LocationData data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveOperation(OperationData data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SavePlace(PlaceData data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExportCSVs(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<AccelerometerData> GetAccelerationData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<LocationData> GetLocationsData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<OperationData> GetOperationsData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<PlaceData> GetPlacesData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- WcfReportsService/ReportsService.svc.cs
+++ WcfReportsService/ReportsService.svc.cs
@@ -8,12 +8,14 @@ using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
 using Lynes.ReportsServer.Core.DataModels;
+using System.Web.Services;
 
 namespace Lynes.ReportsService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, AddressFilterMode = AddressFilterMode.Any)]
+    [WebService(Namespace = "http://Lynes.com/")]
     public class ReportsService : IReportsService
     {
         private DBService m_dbService;
